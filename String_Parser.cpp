a323b6c4e1f4519ad469fb5f7560ec24c55b3366
Kelley, Devon <devon.kelley.r@gmail.com>
2026-05-12 10:32:11 -0700

    Completed Assignment

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -4,7 +4,46 @@
 #include <sstream> // For parsing lines
 using namespace std;
 
-void parseCSV(const string&);
+void parseCSV(const string& filename)
+{
+   ifstream file(filename);
+
+   if(!file.is_open())
+   {
+      cout<<"Error opening file"<<endl;
+      return;
+   }
+
+   string line;
+
+   while (getline(file,line))
+   {
+      stringstream ss(line);
+
+      string name;
+      string age;
+      string initial;
+      string isstudent;
+      string grade;
+
+      getline(ss,name, ' ');
+      getline(ss,age, ' ');
+      getline(ss,initial, ' ');
+      getline(ss,isstudent, ' ');
+      getline(ss,grade, ' ');
+
+      cout<<name<<endl;
+      cout<<age<<endl;
+      cout<<initial<<endl;
+      cout<<isstudent<<endl;
+      cout<<grade<<endl;
+
+      cout<<endl;
+   }
+
+   file.close();
+
+}
 
 int main() {
    parseCSV("students.csv");
